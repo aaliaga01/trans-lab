99ec71a03fcaa4ed9e9303ac02923e02e74f5ea7
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-14 11:33:19 -0300

    empezando a sufrir con js, login inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 /* Holiii acá va tu código también */
+
+$(document).ready(function(){
+
+		 var mail = $("#user-email").val();
+		 var pass = $("#user-pass").val();
+
+    	localStorage.correo = mail;
+    	localStorage.cont = pass;
+
+    $("#iniciar-sesion").click(function() {
+
+    	var claveCorreo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    	var clavePass = /^[a-z][a-z]*/;
+
+        if ( claveCorreo.test(localStorage.correo) && clavePass.test(localStorage.cont)) {
+             window.location.href = 'menu.html'
+        } else {
+            alert("Inténtalo nuevamente");
+        }
+    });
+
+})// ./termina función ready
+
+	
