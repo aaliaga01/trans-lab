c88fc2a8e61c85b4955d2594874bc63cde4b9545
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-14 12:06:29 -0300

    validar login ok, ni yo me lo creo

--- js/main.js
+++ js/main.js
@@ -2,21 +2,20 @@
 
 $(document).ready(function(){
 
-		 var mail = $("#user-email").val();
-		 var pass = $("#user-pass").val();
+    $("#iniciar-sesion").click(function() {
+    	var mail = $("#user-mail").val();
+    	var pass = $("#user-pass").val();
 
     	localStorage.correo = mail;
     	localStorage.cont = pass;
 
-    $("#iniciar-sesion").click(function() {
-
     	var claveCorreo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    	var clavePass = /^[a-z][a-z]*/;
-
-        if ( claveCorreo.test(localStorage.correo) && clavePass.test(localStorage.cont)) {
+    	var clavePass = /^([0-9])*$/;
+		
+        if (claveCorreo.test(mail) && clavePass.test(pass) && pass.length == 8){
              window.location.href = 'menu.html'
         } else {
-            alert("Inténtalo nuevamente");
+            alert("Debes completar todos los campos");
         }
     });
 
