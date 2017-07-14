aa984f47d580d2dbb3658ab6b059ae227a7359b6
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-14 13:07:21 -0300

    se muestra correo ingresado por el usuario

--- js/main.js
+++ js/main.js
@@ -3,22 +3,36 @@
 $(document).ready(function(){
 
     $("#iniciar-sesion").click(function() {
+
+    	//toma los datos ingresados
     	var mail = $("#user-mail").val();
     	var pass = $("#user-pass").val();
 
+    	//los guarda en locastorage
     	localStorage.correo = mail;
     	localStorage.cont = pass;
 
+    	//validaciones 
     	var claveCorreo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     	var clavePass = /^([0-9])*$/;
 		
+		//condicion que revisa las validaciones para poder logearse y ver el menu
         if (claveCorreo.test(mail) && clavePass.test(pass) && pass.length == 8){
              window.location.href = 'menu.html'
         } else {
             alert("Debes completar todos los campos");
         }
+
     });
 
-})// ./termina función ready
+	//-------- Mostrar email del usuario/
+		
+    	var eMail = localStorage.getItem("correo");      
+    	$("#usuario-email span").text(eMail);
 
-	
+
+	//-------- Mostrar email del usuraio/		
+
+
+})// ./termina función ready             
+
